578bccc65a46b7301301167f202ea2e8ecb1f6ed
Zhangziqi <2879310818@qq.com>
2022-09-22 22:22:29 +0800

    增加Comment、Classify两个实体

--- back/src/main/java/priv/togrther/back/entity/Comment.java
+++ back/src/main/java/priv/togrther/back/entity/Comment.java
@@ -1,5 +1,6 @@
 package priv.togrther.back.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -8,5 +9,78 @@ public class Comment {
     @Id
     int comment_id;
 
+    @Column
+    String content;
 
+    @Column
+    Long user_id;
+
+    @Column
+    int forum_id;
+
+    @Column
+    int root_comment_id;
+
+    @Column
+    int to_comment_id;
+
+    public Comment() {
+    }
+
+    public Comment(int comment_id, String content, Long user_id, int forum_id, int root_comment_id, int to_comment_id) {
+        this.comment_id = comment_id;
+        this.content = content;
+        this.user_id = user_id;
+        this.forum_id = forum_id;
+        this.root_comment_id = root_comment_id;
+        this.to_comment_id = to_comment_id;
+    }
+
+    public int getComment_id() {
+        return comment_id;
+    }
+
+    public void setComment_id(int comment_id) {
+        this.comment_id = comment_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getForum_id() {
+        return forum_id;
+    }
+
+    public void setForum_id(int forum_id) {
+        this.forum_id = forum_id;
+    }
+
+    public int getRoot_comment_id() {
+        return root_comment_id;
+    }
+
+    public void setRoot_comment_id(int root_comment_id) {
+        this.root_comment_id = root_comment_id;
+    }
+
+    public int getTo_comment_id() {
+        return to_comment_id;
+    }
+
+    public void setTo_comment_id(int to_comment_id) {
+        this.to_comment_id = to_comment_id;
+    }
 }
