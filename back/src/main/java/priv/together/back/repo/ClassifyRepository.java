40a0fb28fc375f7fef24f561c1cdef92d0dfd48d
Zhangziqi <2879310818@qq.com>
2022-10-18 16:14:57 +0800

    乱七八糟的修改

---
+++ back/src/main/java/priv/together/back/repo/ClassifyRepository.java
@@ -0,0 +1,21 @@
+package priv.together.back.repo;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import priv.together.back.entity.Classify;
+
+@Repository
+public interface ClassifyRepository extends CrudRepository<Classify,Integer> {
+    @Transactional
+    @Modifying
+    @Query(value = "insert into Classify(name) values (?1)",nativeQuery = true)
+    void addNewClassify(String name);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update Classify set name=?1 where classify_id=?2")
+    void modifyClassifyName(String name,int classify_id);
+}
---
+++ back/src/main/java/priv/together/back/repo/CommentRepository.java
@@ -0,0 +1,44 @@
+package priv.together.back.repo;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import priv.together.back.entity.Comment;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends CrudRepository<Comment,Integer> {
+    @Transactional
+    @Modifying
+    @Query(value = "insert into Comment(content,user_id,forum_id) values ( ?1,?2,?3 )",nativeQuery = true)
+    void addNewCommentToForum(String content,Long user_id,int forum_id);
+
+    @Query(value = "select c from Comment c where c.forum_id=?1 and c.root_comment_id=0")
+    List<Comment> getRootCommentByForum_id(int forum_id);
+
+    @Query(value = "select c from Comment c where c.forum_id=?1 and c.root_comment_id=?2")
+    List<Comment> getCommentByForum_idAndRoot_comment_id(int forum_id,int root_comment_id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into Comment(content,user_id,forum_id,root_comment_id) values ( ?1,?2,?3,?4)",nativeQuery = true)
+    void addReplyToComment(String content,Long user_id,int forum_id,int root_comment_id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into Comment(content,user_id,forum_id,root_comment_id,to_comment_id) values ( ?1,?2,?3,?4,?5)",nativeQuery = true)
+    void addReplyToComment(String content,Long user_id,int forum_id,int root_comment_id,int to_comment_id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "delete from Comment  where root_comment_id=?1",nativeQuery = true)
+    void deleteByRoot_comment_id(int root_comment_id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "delete from Comment  where forum_id=?1",nativeQuery = true)
+    void deleteByForum_id(int forum_id);
+}
